82bd4b75fbf79af9b5e284a7b5e0733cc8fed4c8
Rivaldo Ardika Lawalata <nono.qwerty28@gmail.com>
2022-05-16 18:31:27 +0300

    add addRecipe function

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'ff66bde4-a02a-4731-ac10-ab267df1355e';
+export const MODAL_CLOSE_SEC = 2.5;
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,15 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
+  /**
+   * Render the receive object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup is returned if render=false
+   * @this {Object} View instance
+   * @author Rivaldo Lawalata
+   * @todo Finish implementation
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
